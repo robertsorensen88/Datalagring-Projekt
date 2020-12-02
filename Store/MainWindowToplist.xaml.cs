f0ab75be539dee3df422dd1bf66120c2abdf54fb
Lujain Al-shammari <lujain.alshammari@learnet.se>
2020-12-02 19:28:48 +0100

    some change

--- Store/MainWindowToplist.xaml.cs
+++ Store/MainWindowToplist.xaml.cs
@@ -25,6 +25,66 @@ namespace Store
         public MainWindowToplist()
         {
             InitializeComponent();
+            int movie_skip_count = 0;
+            int movie_take_count = 30;
+            State.Movies = API.GetMovieSlice(movie_skip_count, movie_take_count);
+
+            int column_count = MenuBar.ColumnDefinitions.Count;
+
+            int row_count = (int)Math.Ceiling((double)State.Movies.Count / (double)column_count);
+
+            for (int y = 0; y < row_count; y++)
+            {
+                // Skapa en rad-definition för att bestämma hur hög just denna raden är.
+                MenuBar.RowDefinitions.Add(
+                    new RowDefinition()
+                    {
+                        Height = new GridLength(140, GridUnitType.Pixel)
+                    });
+
+                // Lägga till en film i varje cell för en rad
+                for (int x = 0; x < column_count; x++)
+                {
+                    // Räkna ut vilken film vi ska ploppa in härnäst utifrån mina x,y koordinater
+                    int i = y * column_count + x;
+                    // Kolla så att vi inte försöker fylla mer Grid celler än vi har filmrecords.
+                    if (i < State.Movies.Count)
+                    {
+                        // Hämta ett film record
+                        var movie = State.Movies[i];
+
+                        // Försök att skapa en Image Controller(legobit) och
+                        // placera den i rätt Grid cell enl. x,y koordinaterna
+                        // Skapa en Image som visar filmomslaget
+                        var image = new Image()
+                        {
+                            Cursor = Cursors.Hand, // Visa en 'click me' hand när man hovrar över bilden
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(4, 4, 4, 4),
+                        };
+                        
+
+                        try
+                        {
+                            image.Source = new BitmapImage(new Uri(movie.ImageURL)); // Hämta hem bildlänken till RAM
+                        }
+                        catch (Exception e) when (e is ArgumentNullException || e is System.IO.FileNotFoundException || e is UriFormatException)
+                        {
+                            // Om något gick fel så lägger vi in en placeholder 
+                            image.Source = new BitmapImage(new Uri(@"Assets\image-placeholder.jpg"));
+                        }
+
+                        // Lägg till Image i Grid
+                        MenuBar.Children.Add(image);
+
+                        // Placera in Image i Grid
+                        Grid.SetRow(image, y);
+                        Grid.SetColumn(image, x);
+                    }
+                }
+
+            }
         }
 
         private void HomeClick(object sender, RoutedEventArgs e)
